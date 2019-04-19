76b77fd8faef4f27c0e80c26f162843a3d65e400
Sakky4869 <jcjmwh80.private@gmail.com>
2019-04-19 22:29:11 +0900

    タスクオブジェクトの落下と破壊処理の実装
    タスクが落ちてきて、それをタップすると破壊できる処理は実装済み

---
+++ ToDoNoTumori/Assets/Scripts/Hammer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hammer : MonoBehaviour
+{
+    [SerializeField] private AudioSource sePlayer;
+    [SerializeField] private AudioClip[] breakSounds;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject hit = GetRaycastHitObject();
+            if (hit)
+                Destroy(hit);
+        }
+    }
+
+    //タスクオブジェクトの破壊と、破壊音の再生
+    private GameObject GetRaycastHitObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+        if(Physics.Raycast(ray, out raycastHit , 10.0f))
+        {
+            GameObject obj = raycastHit.collider.gameObject;
+            if (obj.tag == "Task")
+            {
+                return obj;
+            }
+        }
+        return null;
+    }
+}
