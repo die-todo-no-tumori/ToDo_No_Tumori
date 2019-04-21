bf78865708a8f01726c5c3287eaa4812ed712f51
Sakky4869 <jcjmwh80.private@gmail.com>
2019-04-21 17:50:36 +0900

    タスクオブジェクトのスクリプトを追加

---
+++ ToDoNoTumori/Assets/Scripts/TaskCreater.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskCreater : MonoBehaviour
+{
+
+    [SerializeField] private Slider prioritySlider;
+
+    void Start()
+    {
+        prioritySlider.onValueChanged.AddListener(delegate { ChangeSliderColorByPriority(); });
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //優先度のスライダーの値に応じてテキストの値を変える
+    private void ChangeSliderColorByPriority()
+    {
+        //テキストの値を変える
+        prioritySlider.transform.parent.GetChild(2).GetComponent<Text>().text = "" + prioritySlider.value;
+    }
+}
---
+++ ToDoNoTumori/Assets/Scripts/TaskObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Task
+{
+    [HideInInspector] public string taskName;
+    [HideInInspector] public int priority;
+    [HideInInspector] public string limit;
+}
+
+public class TaskObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
