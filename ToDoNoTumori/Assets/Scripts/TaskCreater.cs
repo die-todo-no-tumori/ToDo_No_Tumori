ac568b6ce57f7b9909ff02b385451806956489c4
Sakky4869 <jcjmwh80.private@gmail.com>
2019-04-22 09:22:19 +0900

    タスク生成ロジックの作成
    タスクの名前・優先度・期限を入力させて生成ボタンを押すと、そのタスク情報を持ったSphereが生成されるようにした

--- ToDoNoTumori/Assets/Scripts/TaskCreater.cs
+++ ToDoNoTumori/Assets/Scripts/TaskCreater.cs
@@ -7,10 +7,17 @@ public class TaskCreater : MonoBehaviour
 {
 
     [SerializeField] private Slider prioritySlider;
+    [SerializeField] private GameObject taskObject;
+    [SerializeField] private Button taskCreateButton;
 
     void Start()
     {
-        prioritySlider.onValueChanged.AddListener(delegate { ChangeSliderColorByPriority(); });
+        if (prioritySlider != null)
+            prioritySlider.onValueChanged.AddListener(delegate { ChangeSliderColorByPriority(); });
+        else Debug.Log("優先度スライダーはnullです");
+        if (taskCreateButton != null)
+            taskCreateButton.onClick.AddListener(CreateTask);
+        else Debug.Log("タスク生成ボタンはnullです");
     }
 
     void Update()
@@ -23,5 +30,24 @@ public class TaskCreater : MonoBehaviour
     {
         //テキストの値を変える
         prioritySlider.transform.parent.GetChild(2).GetComponent<Text>().text = "" + prioritySlider.value;
+    }
+
+    private void CreateTask()
+    {
+        GameObject taskNameParent = transform.GetChild(0).gameObject;
+        string taskName = taskNameParent.transform.GetChild(1).GetComponent<InputField>().text;
+        GameObject priorityParent = transform.GetChild(1).gameObject;
+        int priority = (int)priorityParent.transform.GetChild(1).GetComponent<Slider>().value;
+        GameObject limitParent = transform.GetChild(2).gameObject;
+        string limit = limitParent.transform.GetChild(1).GetComponent<InputField>().text +
+            "/" + limitParent.transform.GetChild(3).GetComponent<InputField>().text +
+            "/" + limitParent.transform.GetChild(5).GetComponent<InputField>().text +
+            " " + limitParent.transform.GetChild(7).GetComponent<InputField>().text +
+            ":" + limitParent.transform.GetChild(9).GetComponent<InputField>().text;
+        GameObject taskSpawnPosObje = GameObject.Find("TaskSpawnPosObje");
+        Task task = new Task(taskName, priority, limit);
+        GameObject taskObje = Instantiate(taskObject, taskSpawnPosObje.transform.position, Quaternion.identity);
+        taskObje.GetComponent<TaskObject>().task = task;
+        gameObject.SetActive(false);
     }
 }
--- ToDoNoTumori/Assets/Scripts/TaskObject.cs
+++ ToDoNoTumori/Assets/Scripts/TaskObject.cs
@@ -8,10 +8,18 @@ public class Task
     [HideInInspector] public string taskName;
     [HideInInspector] public int priority;
     [HideInInspector] public string limit;
+
+    public Task(string name , int pri, string lim)
+    {
+        this.taskName = name;
+        this.priority = pri;
+        this.limit = lim;
+    }
 }
 
 public class TaskObject : MonoBehaviour
 {
+    [HideInInspector] public Task task;
     void Start()
     {
         
