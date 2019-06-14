1aca2f8781292687f9f412972d15d8b7eb16b52d
Sakky4869 <jcjmwh80.private@gmail.com>
2019-06-14 21:31:10 +0900

    コルーチンをいろいろ試した

---
+++ ToDoNoTumori/Assets/Scripts/CoroutineTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineTest : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        StartCoroutine(Cor1());
+        Debug.Log(4);
+    }
+
+
+
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Cor1()
+    {
+        Debug.Log(1);
+        Test1();
+        Debug.Log(3);
+        yield break;
+    }
+
+    IEnumerator Cor2(int val)
+    {
+        Debug.Log(val);
+        yield break;
+    }
+
+    void Test1()
+    {
+        StartCoroutine(Cor2(2));
+    }
+
+
+}
