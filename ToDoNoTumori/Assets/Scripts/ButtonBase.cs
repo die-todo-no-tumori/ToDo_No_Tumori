c40eb111c8993fe0a039569001befda1924386de
Sakky4869 <jcjmwh80.private@gmail.com>
2019-06-15 17:45:12 +0900

    ボタンの基底クラス・決定ボタンのクラス・キャンセルボタンのクラスを作成
    現状は、音を鳴らすのみ

---
+++ ToDoNoTumori/Assets/Scripts/ButtonBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBase : MonoBehaviour
+{
+    [SerializeField] protected AudioClip push_sound;
+    private AudioSource se_player;
+
+
+    protected virtual void Start()
+    {
+        se_player = Camera.main.gameObject.GetComponent<AudioSource>();
+        GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() =>
+        {
+            PlaySE();
+        });
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void PlaySE()
+    {
+        se_player.PlayOneShot(push_sound);
+    }
+}
