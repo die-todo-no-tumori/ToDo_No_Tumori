d7ae8dea73517950e18dbf2416606c109991a32a
Sakky <45997787+Sakky4869@users.noreply.github.com>
2019-06-18 07:33:55 +0900

    タスクの期限の設定スクリプトファイルの作成
    カレンダーの作成方法について要調査

---
+++ ToDoNoTumori/Assets/Scripts/TaskLimitSetter.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using Syatem.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TaskLimitSetter
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+    
+    public IEnumerator SetTaskLimit(UnityAction<string> callBack)
+    {
+        yield return null;
+    }
+
+}
