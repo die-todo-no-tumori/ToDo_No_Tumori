56f5353e3292175e23bea190f03c889a72da87d1
Sakky4869 <jcjmwh80.private@gmail.com>
2019-06-18 08:58:35 +0900

    カレンダー作成用スクリプトを作成
    カレンダーの大枠は完成
    あとは、日付とかを当てはめていくだけ

---
+++ ToDoNoTumori/Assets/Scripts/CalenderMaker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CalenderMaker : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject calender_cell;
+    [SerializeField]
+    private GameObject spawn_origin;
+
+
+    void Start()
+    {
+        CreateCalender();
+    }
+
+    private void CreateCalender()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                Vector3 pos = spawn_origin.transform.position;
+                pos.x += j * 2;
+                pos.y -= i * 2;
+                Instantiate(calender_cell, pos, Quaternion.identity);
+            }
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- ToDoNoTumori/Assets/Scripts/TaskLimitSetter.cs
+++ ToDoNoTumori/Assets/Scripts/TaskLimitSetter.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections;
-using Syatem.Collections.Generic;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
