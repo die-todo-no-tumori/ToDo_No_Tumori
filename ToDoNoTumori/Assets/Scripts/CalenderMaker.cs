94331200615fd165f0afddea6ab03aa538b25f56
Sakky4869 <jcjmwh80.private@gmail.com>
2019-06-23 02:19:10 +0900

    期限設定画面の作成完了
    モデルも表示できて、ポップアップも出る
    あとは、タスクの玉を出して、入れて、タスク作成するだけ

--- ToDoNoTumori/Assets/Scripts/CalenderMaker.cs
+++ ToDoNoTumori/Assets/Scripts/CalenderMaker.cs
@@ -6,14 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class CalenderMaker : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject calender_cell;
+    //private GameObject calender_cell;
     [SerializeField]
     private GameObject spawn_origin;
     [SerializeField]
     private RectTransform popup_pointer_rect;
-    [SerializeField]
-    private Camera target_camera;
     [SerializeField]
     private LayerMask layerMask;
     private string[] dayOfweek;
@@ -26,50 +23,38 @@ public class CalenderMaker : MonoBehaviour
     private GameObject task_spawn_origin;
     private Vector3 before_touch_pos;
     private GameObject ball;
+    [SerializeField]
+    private GameObject[] calender_cell_objects;
 
     void Start()
     {
-        CreateCalender();
+        CreateCalender(DateTime.Now);
         //StartCoroutine(CreateCalender());
         //taskInputManager = GameObject.Find("TaskInputManager").GetComponent<TaskInputManager>();
         //task_image = taskInputManager.add_task_image;
-        if(task_ball != null)
+        if(task_ball != null && task_spawn_origin != null)
             ball =  Instantiate(task_ball, task_spawn_origin.transform.position, Quaternion.identity);
 
     }
 
-    private void CreateCalender()
+    public void CreateCalender(DateTime dateTime)
     {
         dayOfweek = new string[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
-        CalenderCell[,] calender_cells = new CalenderCell[6, 7];
-
-        for (int i = 0; i < 6; i++)
-        {
-            for(int j = 0; j < 7; j++)
-            {
-                Vector3 pos = spawn_origin.transform.position;
-                pos.x += j * 2;
-                pos.y -= i * 2;
-                GameObject cell = Instantiate(calender_cell, pos, Quaternion.identity);
-                calender_cells[i, j] = cell.GetComponent<CalenderCell>();
-                cell.transform.SetParent(spawn_origin.transform);
-            }
-        }
-
         DateTime first_date_time = new DateTime(DateTime.Now.Year,DateTime.Now.Month, 1);
         DateTime last_date_time = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month));
-        //Debug.Log(last_date_time.Day);
         string first_date = first_date_time.DayOfWeek.ToString();
         int first_index = Array.IndexOf(dayOfweek, first_date);
         int day = 1;
         int date_index = first_index;
         int culum = 0;
+
+        //まずはカレンダーのデータを作成
+        int[,] date_matrix = new int[6, 7];
+
         while(day <= last_date_time.Day)
         {
-            calender_cells[culum, date_index].day = day;
-            calender_cells[culum, date_index].gameObject.GetComponent<MeshRenderer>().material = (Material)Resources.Load("Materials/CC_" + day.ToString("00"));
-            calender_cells[culum, date_index].gameObject.transform.Rotate(0, 180, 0);
+            date_matrix[culum, date_index] = day;
             date_index++;
             if (date_index == 7)
             {
@@ -79,11 +64,21 @@ public class CalenderMaker : MonoBehaviour
             day++;
         }
 
-        foreach(CalenderCell calenderCell in calender_cells)
+        for (int i = 0; i < 6; i++)
         {
-            if(calenderCell.day == 0)
+            for(int j = 0; j < 7; j++)
             {
-                calenderCell.gameObject.SetActive(false);
+                Vector3 pos = spawn_origin.transform.position;
+                pos.x += j * 2;
+                pos.y -= i * 2;
+                GameObject cell = Instantiate(calender_cell_objects[date_matrix[i,j]], pos,spawn_origin.transform.rotation);
+                cell.GetComponent<CalenderCell>().day = date_matrix[i, j];
+                cell.GetComponent<CalenderCell>().date = dayOfweek[j];
+                if(date_matrix[i,j] != 0)
+                {
+                    cell.transform.Rotate(90, 0, 0);
+                }
+                cell.transform.SetParent(spawn_origin.transform);
             }
         }
     }
@@ -114,14 +109,17 @@ public class CalenderMaker : MonoBehaviour
 
             if(Input.GetTouch(0).phase == TouchPhase.Moved || Input.GetTouch(0).phase == TouchPhase.Stationary)
             {
-                Ray ray = target_camera.ScreenPointToRay(Input.GetTouch(0).position);
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
                 RaycastHit hit;
-                if(Physics.Raycast(ray,out hit, 1000,layerMask))
+                //Debug.Log(Input.GetTouch(0).position);
+                //Debug.DrawRay(ray.origin, ray.direction, Color.red, 10,false);
+                if (Physics.Raycast(ray,out hit,100,layerMask))
                 {
+                    //Debug.Log("ray cast hit");
                     if(hit.collider.gameObject.tag == "CalenderCell")
                     {
                         Vector3 pos = popup_pointer_rect.transform.position;
-                        Vector3 hitPos = target_camera.WorldToScreenPoint(hit.collider.transform.position);
+                        Vector3 hitPos = Camera.main.WorldToScreenPoint(hit.collider.transform.position);
                         pos.x = hitPos.x;
                         pos.y = hitPos.y;
                         popup_pointer_rect.transform.position = pos;
@@ -129,17 +127,6 @@ public class CalenderMaker : MonoBehaviour
                         popup_pointer_rect.GetChild(0).GetChild(0).GetComponent<UnityEngine.UI.Text>().text = "" + cell.day;
                         popup_pointer_rect.gameObject.SetActive(true);
                         task_limit = DateTime.Now.Month + ":" + cell.day;
-                        foreach(GameObject cc in GameObject.FindGameObjectsWithTag("CalenderCell"))
-                        {
-                            if(cc.GetComponent<CalenderCell>() == cell)
-                            {
-                                cc.GetComponent<MeshRenderer>().material.color = Color.red;
-                            }
-                            else
-                            {
-                                cc.GetComponent<MeshRenderer>().material.color = Color.white;
-                            }
-                        }
                     }
                 }
             }
