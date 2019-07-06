b97d61e0b03e84a9c74a9a7dd76290548ec153cd
Akihiro <45870449+Akihiro@users.noreply.github.com>
2019-07-06 18:59:48 +0900

    Cubism SDK Test
    Cubism SDKのテストを行いました。

---
+++ ToDoNoTumori/Assets/CubismHitTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Live2D.Cubism.Framework.Raycasting;
+
+public class CubismHitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+        {
+            return;
+        }
+
+        var raycaster = GetComponent<CubismRaycaster>();
+
+        var results = new CubismRaycastHit[4];
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hitCount = raycaster.Raycast(ray, results);
+
+        var resultsText = hitCount.ToString();
+        for(var i = 0; i < hitCount; i++)
+        {
+            resultsText += "\n" + results[i].Drawable.name;
+        }
+
+        Debug.Log(resultsText);
+    }
+}
