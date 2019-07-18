d67596610f84db6e9fab923ce078aa66c046227d
Sakky4869 <jcjmwh80.private@gmail.com>
2019-07-19 08:48:27 +0900

    タップの時間を求めるスクリプトの追加

---
+++ ToDoNoTumori/Assets/Scripts/TapTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TapTimer
+{
+
+	private float total_tap_time;
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+
+
+
+
+}
