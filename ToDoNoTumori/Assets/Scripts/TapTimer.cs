3c5e7e1eadbc166db3bf85dad7fab536d349fa12
Sakky4869 <jcjmwh80.private@gmail.com>
2019-07-19 09:17:00 +0900

    TapTimerを更新

--- ToDoNoTumori/Assets/Scripts/TapTimer.cs
+++ ToDoNoTumori/Assets/Scripts/TapTimer.cs
@@ -7,14 +7,28 @@ public class TapTimer
 {
 
 	private float total_tap_time;
+	private float start_tap_time;
+	private int count;
 	void Start()
 	{
-		
+		count = 0;
 	}
 	
 	void Update()
 	{
-		
+		if(Input.touchCount == 1)
+		{
+			Touch touch = Input.GetTouch(0);
+			if(touch.phase == TouchPhase.Begin)
+			{
+				start_tap_time = Time.time;
+			}
+			else if(touch.phase == TouchPhase.End)
+			{
+				total_tap_time += Time.time - start_tap_time;
+				count++;
+			}
+		}
 	}
 
 
