62f7baea790889e742900a429ae2d7552e1d54e4
Akihiro <45870449+Akihiro@users.noreply.github.com>
2019-07-21 18:09:14 +0900

    Task.csとInputCtoTagC.csの追加
    復旧作業

---
+++ ToDoNoTumori/Assets/InputCtoTagC.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCtoTagC : MonoBehaviour
+{
+    [SerializeField]
+    Task taskScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TaskScriptLauncher()
+    {
+        taskScript.InputCtoTagC();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
