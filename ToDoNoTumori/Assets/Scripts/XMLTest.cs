a1444e7409074c741df7d0952fe0424a133b63cd
yk-ts05 <creagametsuchi@gmail.com>
2019-07-23 21:19:38 +0900

    xmlファイルを追加

---
+++ ToDoNoTumori/Assets/Scripts/XMLTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Xml.Linq;
+
+public class XMLTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        XDocument xDocument = new XDocument(Application.dataPath + "/Resources/commBasic1.xml");
+        r
+        XElement rootElement = xDocument.Element("root");
+
+        var obj = rootElement.Elements("object");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
